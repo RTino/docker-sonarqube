5e5d0259673e93610da0620a658753a4bbdb2b77
Konstantin Manna <Konstantin.Manna@rewe-digital.com>
2016-01-08 14:53:12 +0100

    rewefied the docker-sonarqube image

--- 5.2/run.sh
+++ 5.2/run.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -e
 
-if [ "${1:0:1}" != '-' ]; then
-	exec "$@"
-fi
-
-exec java -jar lib/sonar-application-$SONAR_VERSION.jar \
-  -Dsonar.log.console=true \
-  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
-  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
-  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
-  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
-	"$@"
+exec java -jar "lib/sonar-application-$SONAR_VERSION.jar" \
+		  -Dsonar.log.console=true \
+		  -Dsonar.jdbc.username="$SONARQUBE_JDBC_USERNAME" \
+		  -Dsonar.jdbc.password="$SONARQUBE_JDBC_PASSWORD" \
+		  -Dsonar.jdbc.url="$SONARQUBE_JDBC_URL" \
+		  -Dcrowd.url="$SONARQUBE_CROWD_URL" \
+		  -Dcrowd.application="$SONARQUBE_CROWD_APPLICATION" \
+		  -Dcrowd.password="$SONARQUBE_CROWD_PASSWORD"
+		  -Dsonar.web.javaAdditionalOpts="-Djava.security.egd=file:/dev/./urandom" \
+		  -Dsonar.jdbc.username=sonar \
+		  -Dsonar.jdbc.password=sonar \
+		  -Dsonar.jdbc.maxActive=20 \
+		  -Dsonar.jdbc.maxIdle=5 \
+		  -Dsonar.jdbc.minIdle=2 \
+		  -Dsonar.jdbc.maxWait=5000 \
+		  -Dsonar.jdbc.minEvictableIdleTimeMillis=600000 \
+		  -Dsonar.jdbc.timeBetweenEvictionRunsMillis=30000 \
+		  -Dsonar.web.host='0.0.0.0' \
+		  -Dsonar.web.port=9000 \
+		  -Dsonar.security.realm=Crowd \
+		  -Dsonar.security.localUsers='admin' \
+		  -Dsonar.authenticator.downcase=true \
+		  -Dsonar.security.updateUserAttributes=true \
+		  -Dsonar.security.realm.ignoreStartupFailure=true \
+		  -Dsonar.security.realm.createUsers=true \
